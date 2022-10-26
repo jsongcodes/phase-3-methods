97bac6d5353cf40adc3daf8b69a37d1793681ac6
Jasmin <ssongnari@gmail.com>
2022-10-26 05:59:53 -0400

    commit tree f008cec0a271ea8bf9fbece18f327531af9b42b6

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(integer)
+    if integer.class != Integer
+        return nil
+    end
+
+integer / 2 
+end
